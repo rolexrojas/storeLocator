29baecb1084822e64cf51586822d7fc20ee0451a
Roberto <Robertorojas@outlook.com>
2021-08-03 08:19:13 -0400

    added response for directions service added object mappers for Json Obj

--- src/main/java/com/jumbo/storeLocator/service/Implementation/GoogleMapsService.java
+++ src/main/java/com/jumbo/storeLocator/service/Implementation/GoogleMapsService.java
@@ -1,7 +1,77 @@
 package com.jumbo.storeLocator.service.Implementation;
 
+import com.google.maps.DirectionsApi;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.TravelMode;
+import com.google.maps.model.Unit;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+
 //HERE I WILL COME THE JAVA CLIENT API FOR GOOGLE MAP TO GET INFORMATION OF TOP FIVE CLOSEST ROUTES
+@Service
 public class GoogleMapsService {
+
+    public DistanceMatrix runApiCall() throws InterruptedException, ApiException, IOException {
+
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyBin4uVSYE8jpHGHrmfi_inTDGA_OiUhAA")
+                .build();
+
+        String[] origins =
+                new String[]{
+                        "Sydney, Australia"
+                };
+        String[] destinations =
+                new String[]{
+                        "Uluru, Australia",
+                        "Kakadu, Australia",
+                        "Blue Mountains, Australia",
+                        "Bungle Bungles, Australia",
+                        "The Pinnacles, Australia"
+                };
+
+        DistanceMatrix response = DistanceMatrixApi.newRequest(context)
+                .origins(origins)
+                .destinations(destinations)
+                .mode(TravelMode.DRIVING)
+                .language("en-AU")
+                .avoid(DirectionsApi.RouteRestriction.TOLLS)
+                .units(Unit.IMPERIAL)
+                .departureTime(
+                        Instant.now().plus(Duration.ofMinutes(2))) // this is ignored when an API key is used
+                .await();
+
+        //System.out.println(response.rows);
+
+
+        context.shutdown();
+        return response;
+    }
+
+    public DirectionsResult getDirectionFromPoint2Point(String origin, String destination) throws InterruptedException, ApiException, IOException {
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyBin4uVSYE8jpHGHrmfi_inTDGA_OiUhAA")
+                .build();
+
+        DirectionsResult result =
+                DirectionsApi.getDirections(context, origin, destination)
+                        .mode(TravelMode.DRIVING)
+                        .language("en-EN")
+                        .avoid(DirectionsApi.RouteRestriction.TOLLS)
+                        .units(Unit.IMPERIAL)
+                        .await();
+
+        context.shutdown();
+        return result;
+    }
+
 }
 
 
